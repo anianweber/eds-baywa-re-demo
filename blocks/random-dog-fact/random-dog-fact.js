1041b424eb5868de690077db205d79e2b083af43
Anian Weber <weber.anian@gmail.com>
2024-06-10 13:53:41 +0200

    do stuff

--- blocks/random-dog-fact/random-dog-fact.js
+++ blocks/random-dog-fact/random-dog-fact.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-    const response = await fetch("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1")
-    const data = response.json()
-    console.log(data)
+    const data = "This is an awsome fun fact about dogs! " + Math.random();
+
+    block.innerText = data;
 }
